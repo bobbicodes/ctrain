28cd1a8274cc0dd25164b47e2e152835223036a4
Bobby Towers <sdfwer124@gmail.com>
2017-12-26 03:33:53 -0800

    oops this is the right one

--- src/ctrain/core.clj
+++ src/ctrain/core.clj
@@ -2,17 +2,22 @@
   (:require [clojure.string :as s])
   (:gen-class))
 
+(declare -main)
+
 (def problems
   (read-string (slurp "problems")))
 
 (defn evaluator [n]
+  (let [ans (read-line)]
+    (if (= ans "")
+        (-main))  
   (if (= true
     (eval (read-string (s/replace
-      (first (:tests (nth problems (- n 1)))) "__" (read-line)))))
+      (first (:tests (nth problems (- n 1)))) "__" ans))))
     (do
       (spit "prob" (inc n))
       (println "")
-      (str "GOOD JOB"))))
+      (str "GOOD JOB")))))
 
 (defn problem [n]
   (println (str "#" n " " (:title (nth problems (- n 1)))))
