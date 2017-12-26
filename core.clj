e79717138b8b3e96a10d9391d3789101d65ec131
Bobby Towers <sdfwer124@gmail.com>
2017-12-26 02:15:53 -0800

    reject empty string

---
+++ core.clj
@@ -0,0 +1,33 @@
+(ns ctrain.core
+  (:require [clojure.string :as s])
+  (:gen-class))
+
+(declare -main)
+
+(def problems
+  (read-string (slurp "problems")))
+
+(defn evaluator [n]
+  (let [ans (read-line)]
+    (if (= ans "")
+        (-main))  
+  (if (= true
+    (eval (read-string (s/replace
+      (first (:tests (nth problems (- n 1)))) "__" ans))))
+    (do
+      (spit "prob" (inc n))
+      (println "")
+      (str "GOOD JOB")))))
+
+(defn problem [n]
+  (println (str "#" n " " (:title (nth problems (- n 1)))))
+  (println "")
+   (println (str (:description (nth problems (- n 1)))))
+   (println "")
+  (println (first (:tests (nth problems (- n 1)))))
+  (println (evaluator n)))
+
+(defn -main []
+  (println "")
+  (problem (read-string (slurp "prob")))
+  (-main))
