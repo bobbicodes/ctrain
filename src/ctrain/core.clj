5e211f9e1ae8fa5e654b4e3319303903c3756ed1
Bobby Towers <sdfwer124@gmail.com>
2017-12-27 04:04:14 -0800

    rolling back - broke it

--- src/ctrain/core.clj
+++ src/ctrain/core.clj
@@ -7,39 +7,27 @@
 (def problems
   (read-string (slurp "problems")))
 
-(defn replacer [n]
-  (let [ans (read-line)] 
-(loop [tests (:tests (problems (- n 1))) replaced []]
-  (if (empty? tests)
-      replaced
-      (recur (rest tests) (conj replaced (s/replace (first tests) "__" ans)))))))
-
-(defn evaluator [answers]
-  (loop [totest answers results []]
-    (if (empty? totest)
-      results
-      (recur (rest totest) (conj results (eval (read-string (first totest))))))))
-
-(defn final [results]
-  (loop [coll results]
-    (if (empty? coll)
-      (do
-      (spit "prob" (inc (read-string (slurp "prob"))))
+(defn evaluator [n]
+  (let [ans (read-line)]
+    (if (= ans "")
+        (-main))  
+  (if (= true
+    (eval (read-string (s/replace
+      (first (:tests (nth problems (- n 1)))) "__" ans))))
+    (do
+      (spit "prob" (inc n))
       (println "")
-      (str "GOOD JOB")
-      (-main)))
-   (if (= false (first coll))
-       (-main))      
-(recur (rest coll))))
+      (str "GOOD JOB")))))
 
 (defn problem [n]
   (println (str "#" n " " (:title (nth problems (- n 1)))))
   (println "")
    (println (str (:description (nth problems (- n 1)))))
    (println "")
-   (run! println (:tests (nth problems (- n 1)))))
+   (run! println (:tests (nth problems (- n 1))))
+  (println (evaluator n)))
 
 (defn -main []
   (println "")
-  (final (evaluator (replacer (problem (read-string (slurp "prob")))
-  (-main)))))
+  (problem (read-string (slurp "prob")))
+  (-main))
