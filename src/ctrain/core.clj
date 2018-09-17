5fadbea52f3da219fbfd81fe70d92b1b6501439c
kazesberger <klaus.azesberger@gmail.com>
2018-09-17 23:08:10 +0200

    Treat eval Exceptions as failed testresults

--- src/ctrain/core.clj
+++ src/ctrain/core.clj
@@ -12,22 +12,30 @@
   (loop [coll results]
     (if (empty? coll)
       (do
-      (spit "prob" (inc (read-string (slurp "prob"))))
-      (println "\nGOOD JOB! Here's the next one:")
+       (spit "prob" (inc (read-string (slurp "prob"))))
+       (println "\nGOOD JOB! Here's the next one:")
        (Thread/sleep 1000)
-      (-main)))
-   (if (= false (first coll))
+       (-main)))
+    (if (= false (first coll))
        (do
           (println "\nNope... try again or Ctrl+C to quit")
           (Thread/sleep 1000)
-       (-main)))
-(recur (rest coll))))
+          (-main)))
+    (recur (rest coll))))
 
 (defn evaluator [answers]
   (loop [totest answers results []]
     (if (empty? totest)
       (final results)
-      (recur (rest totest) (conj results (eval (read-string (first totest))))))))
+      (recur
+        (rest totest)
+        (conj results
+          (try
+             (eval (read-string (first totest)))
+             (catch Exception e
+               (println (str "Error evaluating: " (class e) ":" (.getMessage e)))
+               (.printStackTrace e)
+               false)))))))
 
 (defn replacer [n]
   (let [ans (slurp (str "ans-" n))]
@@ -36,19 +44,19 @@
           (println "Nice try, but blank answers are not allowed.")
           (Thread/sleep 1000)
           (-main)))
-(loop [tests (:tests (problems (- n 1))) replaced []]
-  (if (empty? tests)
-      (evaluator replaced)
-      (recur (rest tests) (conj replaced (s/replace (first tests) "__" ans)))))))
+    (loop [tests (:tests (problems (- n 1))) replaced []]
+        (if (empty? tests)
+          (evaluator replaced)
+          (recur (rest tests) (conj replaced (s/replace (first tests) "__" ans)))))))
 
 
 (defn problem [n]
   (println (str "\n#" n ": " (:title (nth problems (- n 1)))))
-   (println (str "\n" (:description (nth problems (- n 1)))) "\n")
-   (run! println (:tests (nth problems (- n 1))))
-   (spit (str "ans-" n)(read-line)))
+  (println (str "\n" (:description (nth problems (- n 1)))) "\n")
+  (run! println (:tests (nth problems (- n 1))))
+  (spit (str "ans-" n)(read-line)))
 
 (defn -main []
   (let [n (read-string (slurp "prob"))]
     (problem n)
-  (replacer n)))
+    (replacer n)))
