d362b92f08f985e3fe6e89f017cd8ac9dc7ec128
Bobby Towers <porkostomus@mail.com>
2018-10-07 14:21:27 -0700

    add prob-num test

--- test/ctrain/core_test.clj
+++ test/ctrain/core_test.clj
@@ -16,3 +16,16 @@
                                             :description "Clojure strings are Java strings, so you can use Java string methods on them."}]]
                              (get-answer data 2))))) 
 
+(deftest prob-num-test
+  (is (= 2 (let [data [{:_id 1, :title "Nothing but the Truth"
+                                           :tests ["(= __ true)"]
+                                           :description "Complete the expression so it will evaluate to true."
+                                           :answer "true"}
+                                          {:_id 2, :title "Simple Math"
+                                            :tests ["(= (- 10 (* 2 3)) __)"]
+                                            :description "Innermost forms are evaluated first."
+                                            :answer "4"}
+                                          {:_id 3, :title "Strings"
+                                            :tests ["(= __ (.toUpperCase \"eat me\"))"]
+                                            :description "Clojure strings are Java strings, so you can use Java string methods on them."}]]
+                       (prob-num data)))))
