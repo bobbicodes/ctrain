0a722cad89b8b26f858fe18f321dfedad8ad4d38
Bobby Towers <porkostomus@mail.com>
2018-10-07 16:00:12 -0700

    add get-current-answer fn and test passes

--- test/ctrain/core_test.clj
+++ test/ctrain/core_test.clj
@@ -2,8 +2,8 @@
   (:require [clojure.test :refer :all]
             [ctrain.core :refer :all]))
 
-(deftest get-answer-test
-  (is (= "4" (let [data [{:_id 1, :title "Nothing but the Truth"
+(def data
+  [{:_id 1, :title "Nothing but the Truth"
                                            :tests ["(= __ true)"]
                                            :description "Complete the expression so it will evaluate to true."
                                            :answer "true"}
@@ -13,19 +13,13 @@
                                             :answer "4"}
                                           {:_id 3, :title "Strings"
                                             :tests ["(= __ (.toUpperCase \"eat me\"))"]
-                                            :description "Clojure strings are Java strings, so you can use Java string methods on them."}]]
-                             (get-answer data 2))))) 
+                                            :description "Clojure strings are Java strings, so you can use Java string methods on them."}])
+
+(deftest get-answer-test
+  (is (= "4" (get-answer data 2))))
 
 (deftest prob-num-test
-  (is (= 2 (let [data [{:_id 1, :title "Nothing but the Truth"
-                                           :tests ["(= __ true)"]
-                                           :description "Complete the expression so it will evaluate to true."
-                                           :answer "true"}
-                                          {:_id 2, :title "Simple Math"
-                                            :tests ["(= (- 10 (* 2 3)) __)"]
-                                            :description "Innermost forms are evaluated first."
-                                            :answer "4"}
-                                          {:_id 3, :title "Strings"
-                                            :tests ["(= __ (.toUpperCase \"eat me\"))"]
-                                            :description "Clojure strings are Java strings, so you can use Java string methods on them."}]]
-                       (prob-num data)))))
+  (is (= 2 (prob-num data))))
+
+(deftest get-current-answer-test
+  (is (= "4" (get-current-answer data))))
