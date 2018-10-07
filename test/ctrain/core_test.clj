3605d048b4b59372a7e1a47a512045f1d2cbdb29
Bobby Towers <porkostomus@mail.com>
2018-10-07 13:59:25 -0700

    get-answer test

--- test/ctrain/core_test.clj
+++ test/ctrain/core_test.clj
@@ -2,8 +2,17 @@
   (:require [clojure.test :refer :all]
             [ctrain.core :refer :all]))
 
-(deftest get-problem-test
-  (is (= (get-problem 1)
-         {:_id 1, :title "Nothing but the Truth"
-          :tests ["(= __ true)"]
-          :description "Complete the expression so it will evaluate to true."})))
+(deftest get-answer-test
+  (is (= "4" (let [data [{:_id 1, :title "Nothing but the Truth"
+                                           :tests ["(= __ true)"]
+                                           :description "Complete the expression so it will evaluate to true."
+                                           :answer "true"}
+                                          {:_id 2, :title "Simple Math"
+                                            :tests ["(= (- 10 (* 2 3)) __)"]
+                                            :description "Innermost forms are evaluated first."
+                                            :answer "4"}
+                                          {:_id 3, :title "Strings"
+                                            :tests ["(= __ (.toUpperCase \"eat me\"))"]
+                                            :description "Clojure strings are Java strings, so you can use Java string methods on them."}]]
+                             (get-answer data 2))))) 
+
