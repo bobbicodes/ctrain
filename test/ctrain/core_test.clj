02dfaf31d49b0652adcc36de44e78770212d88d6
Bobby Towers <porkostomus@mail.com>
2018-10-08 13:37:03 -0700

    add test for incorrect answer

--- test/ctrain/core_test.clj
+++ test/ctrain/core_test.clj
@@ -16,6 +16,20 @@
                      "(= __ (clojure.set/union #{:a :b :c} #{:b :c :d}))"]
       :description "Sets are collections of unique values."}])
 
+(def data-false
+  [{:_id 6, :title "Vectors"
+     :tests ["(= [__] (list :a :b :c) (vec '(:a :b :c)) (vector :a :b :c))"]
+     :description "Vectors can be constructed several ways.  You can compare them with lists."
+      :answer ":a :b :c"}
+    {:_id 7, :title "conj on vectors"
+      :tests ["(= __ (conj [1 2 3] 4))" "(= __ (conj [1 2] 3 4))"]
+      :description "When operating on a Vector, the conj function will return a new vector with one or more items \"added\" to the end."
+      :answer "[1 2 3]"}
+    {:_id 8, :title "Sets"
+      :tests ["(= __ (set '(:a :a :b :c :c :c :c :d :d)))"
+                     "(= __ (clojure.set/union #{:a :b :c} #{:b :c :d}))"]
+      :description "Sets are collections of unique values."}])
+
 (deftest get-answer-test
   (is (= "[1 2 3 4]" (get-answer data 2))))
 
@@ -35,4 +49,6 @@
 
 (deftest evaluate-test
   (is (= [true true]
-              (evaluate (read-string (submit data))))))
+              (evaluate (read-string (submit data)))))
+  (is (= [false false]
+              (evaluate (read-string (submit data-false))))))
