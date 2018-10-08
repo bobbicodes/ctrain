79b305301c1f2fce7ef0ec3bfe7c0a496b818371
Bobby Towers <porkostomus@mail.com>
2018-10-07 18:58:44 -0700

    get-tests, other-data

--- test/ctrain/core_test.clj
+++ test/ctrain/core_test.clj
@@ -15,6 +15,20 @@
                                             :tests ["(= __ (.toUpperCase \"eat me\"))"]
                                             :description "Clojure strings are Java strings, so you can use Java string methods on them."}])
 
+(def other-data
+  [{:_id 6, :title "Vectors"
+     :tests ["(= [__] (list :a :b :c) (vec '(:a :b :c)) (vector :a :b :c))"]
+     :description "Vectors can be constructed several ways.  You can compare them with lists."
+      :answer ":a :b :c"}
+    {:_id 7, :title "conj on vectors"
+      :tests ["(= __ (conj [1 2 3] 4))" "(= __ (conj [1 2] 3 4))"]
+      :description "When operating on a Vector, the conj function will return a new vector with one or more items \"added\" to the end."
+      :answer "[1 2 3 4]"}
+    {:_id 8, :title "Sets"
+      :tests ["(= __ (set '(:a :a :b :c :c :c :c :d :d)))"
+                     "(= __ (clojure.set/union #{:a :b :c} #{:b :c :d}))"]
+      :description "Sets are collections of unique values."}])
+
 (deftest get-answer-test
   (is (= "4" (get-answer data 2))))
 
@@ -23,3 +37,7 @@
 
 (deftest get-current-answer-test
   (is (= "4" (get-current-answer data))))
+
+(deftest get-tests-test
+  (is (= ["(= __ (conj [1 2 3] 4))" "(= __ (conj [1 2] 3 4))"]
+             (get-tests other-data))))
