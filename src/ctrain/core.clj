0d74adb7b1c18200ac83ca837fa83e7b5cd3fe49
porkostomus <porkostomus@mail.com>
2020-03-05 21:09:19 -0800

    refactor

--- src/ctrain/core.clj
+++ src/ctrain/core.clj
@@ -1,55 +1,65 @@
 (ns ctrain.core
   (:require 
    [clojure.string :as str]
+   [clojure.set]
    [ctrain.problems :refer [problems]]))
 
 (declare -main)
 
+(defn prompt []
+  (let [n (read-string (slurp "resources/prob-num"))]
+    (println (str "\n#" n ": " (:title (nth problems (dec n)))))
+    (println (str "\n" (:description (nth problems (dec n))) "\n"))
+    (run! println (:tests (nth problems (dec n))))
+    (spit (str "resources/answers/" n) (read-line))))
+
+(defn reject []
+  (println "\nSorry, try again...")
+  (Thread/sleep 1500)
+  (-main))
+
+(defn advance-prob-num! []
+  (spit "resources/prob-num"
+        (inc (read-string (slurp "resources/prob-num")))))
+
+(defn pass []
+  (println "\nNICE! Here's the next one:")
+  (Thread/sleep 1500)
+  (advance-prob-num!)
+  (-main))
+
 (defn check [results]
-    (when (empty? results)
-      (spit "resources/prob-num" (inc (read-string (slurp "resources/prob-num"))))
-      (println "\nNICE! Here's the next one:")
-      (Thread/sleep 1500)
-      (-main))
-    (when (= false (first results))
-      (println "\nSorry, try again...")
-      (Thread/sleep 1500)
-      (-main))
-    (recur (rest results)))
+  (cond (empty? results) (pass)
+        (= false (first results)) (reject)
+        :else (recur (rest results))))
 
 (defn evaluate [answers]
-  (loop [answers answers results []]
+  (loop [answers answers
+         results []]
     (if (empty? answers)
       (check results)
       (recur
-        (rest answers)
-        (conj results
-          (try
-             (eval (read-string (first answers)))
-             (catch Exception e
-               (println (str "Error evaluating: " (class e) ":" (.getMessage e)))
-               (.printStackTrace e)
-               false)))))))
+       (rest answers)
+       (conj results
+             (try
+               (eval (read-string (first answers)))
+               (catch Exception e
+                 (println (str "Error evaluating: " (class e) ":" (.getMessage e)))
+                 (.printStackTrace e)
+                 false)))))))
 
-(defn submit [n]
-  (let [ans (slurp (str "resources/answers/" n))]
-    (when (= ans "")
-          (-main))
-    (loop [tests (:tests (problems (- n 1))) replaced []]
-        (if (empty? tests)
-          (evaluate replaced)
-          (recur (rest tests) (conj replaced (str/replace (first tests) "__" ans)))))))
-
-(defn prompt [n]
-  (println (str "\n#" n ": " (:title (nth problems (dec n)))))
-  (println (str "\n" (:description (nth problems (dec n))) "\n"))
-  (run! println (:tests (nth problems (dec n))))
-  (spit (str "resources/answers/" n)(read-line)))
+(defn submit []
+  (let [n (read-string (slurp "resources/prob-num"))
+        ans (slurp (str "resources/answers/" n))]
+    (loop [tests    (:tests (problems (- n 1)))
+           replaced []]
+      (if (empty? tests)
+        (evaluate replaced)
+        (recur (rest tests) (conj replaced (str/replace (first tests) "__" ans)))))))
 
 (defn -main []
-  (let [n (read-string (slurp "resources/prob-num"))]
-    (prompt n)
-    (submit n)))
+  (prompt)
+  (submit))
 
 (comment
-  (-main))
+)
