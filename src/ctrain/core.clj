f15640685c992cef2fd61db02d74cb0a4a5e9adb
porkostomus <porkostomus@mail.com>
2020-03-05 22:04:09 -0800

    reject empty string

--- src/ctrain/core.clj
+++ src/ctrain/core.clj
@@ -18,18 +18,15 @@
   (Thread/sleep 1500)
   (-main))
 
-(defn advance-prob-num! []
-  (spit "resources/prob-num"
-        (inc (read-string (slurp "resources/prob-num")))))
-
-(defn pass []
+(defn next-prob! []
   (println "\nNICE! Here's the next one:")
   (Thread/sleep 1500)
-  (advance-prob-num!)
+  (spit "resources/prob-num"
+        (inc (read-string (slurp "resources/prob-num"))))
   (-main))
 
 (defn check [results]
-  (cond (empty? results) (pass)
+  (cond (empty? results) (next-prob!)
         (= false (first results)) (reject)
         :else (recur (rest results))))
 
@@ -42,11 +39,13 @@
 (defn submit []
   (let [n (read-string (slurp "resources/prob-num"))
         ans (slurp (str "resources/answers/" n))]
-    (loop [tests    (:tests (problems (- n 1)))
-           replaced []]
-      (if (empty? tests)
-        (check (reduce conj [] (map safe-eval replaced)))
-        (recur (rest tests) (conj replaced (str/replace (first tests) "__" ans)))))))
+    (if (= ans "")
+      (reject)
+      (loop [tests    (:tests (problems (- n 1)))
+             replaced []]
+        (if (empty? tests)
+          (check (reduce conj [] (map safe-eval replaced)))
+          (recur (rest tests) (conj replaced (str/replace (first tests) "__" ans))))))))
 
 (defn -main []
   (prompt)
